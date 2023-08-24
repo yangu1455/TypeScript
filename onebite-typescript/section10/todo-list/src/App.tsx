bf61c7e29660da8c1d818bad869b8057de5f1f7c
yangu1455 <kunja266@hanmail.net>
2023-08-24 20:46:15 +0900

    useReducer 사용하기

--- onebite-typescript/section10/todo-list/src/App.tsx
+++ onebite-typescript/section10/todo-list/src/App.tsx
@@ -1,30 +1,56 @@
-import { useState, useRef, useEffect } from 'react'
+import { useState, useRef, useEffect, useReducer } from 'react'
 import './App.css'
 import Editor from './components/Editor';
 import { Todo } from './types';
 import TodoItem from './components/TodoItem';
 
+type Action = 
+| {
+  type: "CREATE",
+  data: {
+    id: number;
+    content: string;
+  }
+} 
+| { 
+  type: "DELETE"; id: number 
+};
+
+function reducer(state: Todo[], action: Action) {
+  switch(action.type){
+    case 'CREATE': {
+      return [...state, action.data];
+    }
+    case'DELETE': {
+      return state.filter((it) => it.id !== action.id);
+    }
+  }
+}
+
 function App() {
   // todolist의 타입은 Todo의 배열 타입
-  const [todolist, setTodolist] = useState<Todo[]>([]);
+  const [todolist, dispatch] = useReducer(reducer, []);
   
   const idRef = useRef(1);
 
   // 추가 버튼을 누를 때 실행 시킬 것
   // setTodolist 호출때문에 옮기지 않는다.
   const onClickAdd = (text: string) => {
-    setTodolist([
-      ...todolist,
-      {
+    dispatch({
+      type: "CREATE",
+      data: {
         // idRef 현재 값에 ++를 해준당
         id: idRef.current++,
         content: text,
       }
-    ])
+    })
   }
 
   const onClickDelete = (id: number) => {
-    setTodolist(todolist.filter((todo) => todo.id !== id));
+    dispatch({
+      type: "DELETE",
+      id: id,
+    })
   }
 
   useEffect(()=>{
