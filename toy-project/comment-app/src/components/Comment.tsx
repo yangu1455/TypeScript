cb1c511be89f54652cd8d0f9f31cd628f4e27e3c
yangu1455 <kunja266@hanmail.net>
2023-09-11 23:49:09 +0900

    css랑 댓글 수정 방식을 수정

--- toy-project/comment-app/src/components/Comment.tsx
+++ toy-project/comment-app/src/components/Comment.tsx
@@ -9,11 +9,14 @@ const today = dateNow.toISOString().slice(0, 10);
 const Comment = (): JSX.Element => {
   // text를 받아오는 고런..
   const [newText, setNewText] = useState('');
+  const [newEditText, setNewEditText] = useState('');
+  // 댓글 생성시..
   const [comments, setComments] = useState<commentType[]>([]);
   // 수정 기능을 위해 대상 댓글의 ID 받아오는..
   const [editingCommentId, setEditingCommentId] = useState<number | null>(null);
   // 댓글 내용 포커싱을 위한 useRef
   const CommentTextInput: React.MutableRefObject< | HTMLInputElement | undefined> = useRef();
+  const CommentEditTextInput: React.MutableRefObject< | HTMLInputElement | undefined> = useRef();
 
   useEffect(() => {
     // JSON 파일을 비동기적으로 로드
@@ -36,7 +39,7 @@ const Comment = (): JSX.Element => {
 
     // text 제외한 모든 고정된 값을 가진 댓글을 생성
     const newComment = {
-      id: comments.length + 1,
+      id: comments.length,
       name: 'yangu1455',
       text: newText,
       timestamp: new Date(),
@@ -45,47 +48,23 @@ const Comment = (): JSX.Element => {
 
     // 새로운 댓글을 기존 댓글 목록에 추가
     setComments([...comments, newComment]);
-    console.log(newComment);
     setNewText('');
-
-    // // JSON 파일을 불러와서 기존 데이터에 새로운 댓글을 추가합니다.
-    // fetch('/src/comments.json') // Vite는 정적 리소스를 `/src` 경로에서 제공합니다.
-    //   .then((response) => response.json())
-    //   .then((data) => {
-    //     const newComments = [...data, newComment];
-    //     console.log(newComments)
-    //     return newComments;
-    //   })
-    //   .then((newComments) => {
-    //     // JSON 파일로 업데이트합니다.
-    //     return fetch('/src/comments.json', {
-    //       method: 'POST',
-    //       headers: {
-    //         'Content-Type': 'application/json',
-    //       },
-    //       body: JSON.stringify(newComments),
-    //     });
-    //   })
-    //   .then(() => {
-    //     console.log('새로운 댓글이 성공적으로 추가되었습니다.');
-    //     setNewText('');
-    //   })
-    //   .catch((error) => console.error('댓글을 추가하는 중 오류 발생:', error));
-
   };
-
-
 
   const editCommentSubmit = (id: number, text: string) => {
     setEditingCommentId(id);
-    setNewText(text);
+    setNewEditText(text);
+    // 안먹힘..
+    // if (CommentEditTextInput.current) {
+    //   CommentEditTextInput.current.focus();
+    // }
   };
 
   const handleConfirmEdit = () => {
-    if (newText.length === 0 || editingCommentId === null) {
+    if (newEditText.length === 0 || editingCommentId === null) {
       // 댓글 내용이 비어있거나 수정 대상 댓글이 없으면 수정하지 않음
-      if (CommentTextInput.current) {
-        CommentTextInput.current.focus();
+      if (CommentEditTextInput.current) {
+        CommentEditTextInput.current.focus();
       }
       return;
     }
@@ -95,7 +74,7 @@ const Comment = (): JSX.Element => {
         return {
           ...comment,
           // 새로 받을 text
-          text: newText,
+          text: newEditText,
           // 수정된 시간
           timestamp: new Date(),
         };
@@ -104,7 +83,7 @@ const Comment = (): JSX.Element => {
     });
 
     setComments(updatedComments);
-    setNewText('');
+    setNewEditText('');
     setEditingCommentId(null);
   };
 
@@ -139,32 +118,59 @@ const Comment = (): JSX.Element => {
 
   return (
     <div className='comment'>
-      <h1>{today}</h1>
+      <h1 className='today'>{today}</h1>
       <ul>
         {comments.map((comment) => (
           <li key={comment.id}>
             <div className='comment-profile-timestamp-box'>
               <div className='comment-profile-box'>
-                <img className='comm_pf_pic' src={comment.pf_pic} alt="프로필 사진" />
-                <p>{comment.name}</p>
+                <img className='comment-pf-pic' src={comment.pf_pic} alt="프로필 사진" />
+                <p className='comment-name'>{comment.name}</p>
               </div>
               <div>
                 <p>{elapsedTime(comment.timestamp)}</p>
               </div>
             </div>
-            <div className='comment-content'>
-              <p>{comment.text}</p>
+
+
+            <div className='comment-edit-content'>
+              {/* // 수정 모드 일때 */}
+              {editingCommentId === comment.id ? (
+                <div>
+                  <input
+                    ref={CommentEditTextInput as React.MutableRefObject<HTMLInputElement>}
+                    className='comment-edit-input'
+                    type='text'
+                    placeholder="댓글을 입력하세요"
+                    value={newEditText}
+                    onChange={(e) => setNewEditText(e.target.value)}
+                  />
+                </div>
+              ) : ( 
+                // 수정 모드 아닐 때
+                <div>
+                  <p className='comment-text'>{comment.text}</p>
+                </div>
+              )}
               <div>
                 {editingCommentId === comment.id ? (
                   // 수정 모드 일때
-                  <button onClick={() => setEditingCommentId(null)}>취소</button>
+                  <div>
+                    <button onClick={handleConfirmEdit}>확인</button>
+                    <button onClick={() => setEditingCommentId(null)}>취소</button>
+                    <button onClick={() => deleteCommentSubmit(comment.id)}>삭제</button>
+                  </div>
                 ) : ( 
                   // 수정 모드 아닐 때
-                  <button onClick={() => editCommentSubmit(comment.id, comment.text)}>수정</button>
+                  <div>
+                    <button onClick={() => editCommentSubmit(comment.id, comment.text)}>수정</button>
+                    <button onClick={() => deleteCommentSubmit(comment.id)}>삭제</button>
+                  </div>
                 )}
-                <button onClick={() => deleteCommentSubmit(comment.id)}>삭제</button>
               </div>
             </div>
+
+
           </li>
         ))}
       </ul>
@@ -177,9 +183,8 @@ const Comment = (): JSX.Element => {
           value={newText}
           onChange={(e) => setNewText(e.target.value)}
         />
-        {/* 수정 모드일 경우에는 확인 & 수정 기능 / 수정 모드 아닐 경우에는 댓글 생성 & 댓글 생성 기능 */}
-        <button onClick={editingCommentId !== null ? handleConfirmEdit : handleCommentSubmit}>
-          {editingCommentId !== null ? '확인' : '댓글 생성'}
+        <button onClick={handleCommentSubmit}>
+          댓글 생성
         </button>
       </div>
     </div>
