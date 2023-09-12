b183dfc3a01d53b55c116d3c2c835483a21ff8b9
yangu1455 <kunja266@hanmail.net>
2023-09-13 02:33:20 +0900

    비동기처리 부분 수정필요... 꼬옥

--- toy-project/comment-app/src/components/Tictoc.tsx
+++ toy-project/comment-app/src/components/Tictoc.tsx
@@ -4,7 +4,7 @@ import './Tictoc.css';
 
 // dummydata를 만들어서..
 // 잘 작동하는지 보자구......
-// 
+
 const today_is = new Date().toISOString().slice(0, 10);
 
 const Tictoc = () => {
@@ -15,24 +15,47 @@ const Tictoc = () => {
   // intervalId를 받아 현재 시간 측정중인지!
   const [intervalId, setIntervalId] = useState<number | null>(null);
   
-
   // 버튼 클릭 시 시간 측정 시작
   const startTimer = () => {
-    // 시작 시간 새로 받기
-    setStartTime(new Date());
+    // 시간 측정 중이 아니라면!
     if (intervalId === null) {
+      // 시작 시간 새로 받기
+      setStartTime(new Date());
       // setInterval 시작하고 인터벌 ID를 저장
       const id = window.setInterval(() => {
-      const currentTime = new Date();
-      // null 체크 빼먹지 말자,,
-      const elapsedMilliseconds = startTime ? currentTime.getTime() - startTime.getTime() : 0;
-      setElapsedTime(elapsedMilliseconds);
-    }, 1000);
-    setIntervalId(id); // 인터벌 ID 저장
-    } else {
-      return;
+        const currentTime = new Date();
+        // null 체크 빼먹지 말자,,
+        const elapsedMilliseconds = startTime ? currentTime.getTime() - startTime.getTime() : 0;
+        setElapsedTime(elapsedMilliseconds);
+      }, 1000);
+      setIntervalId(id); // 인터벌 ID 저장
     }
   };
+  
+  const timepause = () => {
+    // setInterval을 중지하고 intervalId를 초기화 시킨다
+    if (intervalId) {
+      // setStartTime(null);
+      clearInterval(intervalId);
+      setIntervalId(null);
+    }
+    // 그리고 기록된 시간 저장하기
+    // 같은 날의 기록이 있다면 studytime에 더해주기만 하고
+    // 없다면 새로운 기록으로 등록해주기
+  }
+
+  // 비동기처리... 수정해야할거같습니뎅
+  // useEffect(() => {
+  //   // 시작 시간이 변경될 때마다 경과 시간 계산
+  //   if (startTime) {
+  //     const interval = setInterval(() => {
+  //       const currentTime = new Date();
+  //       const elapsedMilliseconds = currentTime.getTime() - startTime.getTime();
+  //       setElapsedTime(elapsedMilliseconds);
+  //     }, 1000);
+  //     return () => clearInterval(interval);
+  //   }
+  // }, [startTime]);
 
   // 밀리초를 시, 분, 초로 변환
   const millisecondsToTime = (milliseconds: number) => {
@@ -49,17 +72,6 @@ const Tictoc = () => {
 
   // 저장된 시간 포맷에~
   const formattedTime = millisecondsToTime(elapsedTime);
-
-  const timepause = () => {
-    // setInterval을 중지하고 intervalId를 초기화 시킨다
-    if (intervalId) {
-      clearInterval(intervalId);
-      setIntervalId(null);
-    }
-    // 그리고 기록된 시간 저장하기
-    // 같은 날의 기록이 있다면 studytime에 더해주기만 하고
-    // 없다면 새로운 기록으로 등록해주기
-  }
 
   return (
     <div>
