7103dc6ba83709d475feea20203421444122982f
yangu1455 <kunja266@hanmail.net>
2023-09-16 17:32:01 +0900

    fix: 저장 기능 수정

--- toy-project/comment-app/src/components/Tictoc.tsx
+++ toy-project/comment-app/src/components/Tictoc.tsx
@@ -2,9 +2,6 @@ import { useState, useEffect } from 'react';
 import { BsFillPlayCircleFill, BsFillPauseCircleFill } from 'react-icons/bs';
 import './Tictoc.css';
 
-// dummydata를 만들어서..
-// 잘 작동하는지 보자구......
-
 const today_is = new Date().toISOString().slice(0, 10);
 
 const Tictoc = () => {
@@ -14,6 +11,11 @@ const Tictoc = () => {
   const [elapsedTime, setElapsedTime] = useState<number>(0);
   // intervalId를 받아 현재 시간 측정중인지!
   const [intervalId, setIntervalId] = useState<number | null>(null);
+
+  // 현재 날짜를 문자열로 가져옵니다.
+  const currentDate = new Date().toISOString().slice(0, 10);
+  // localStorage에서 해당 날짜의 데이터를 가져옵니다.
+  const storedData = localStorage.getItem(currentDate);
   // 저장된 공부시간 가져오기!
   const savedStudyTime = localStorage.getItem('studyTime');
   // 문자열을 10진수 정수 number로 변환합니다.
@@ -24,23 +26,36 @@ const Tictoc = () => {
   const startTimer = () => {
     // 시간 측정 중이 아니라면!
     if (intervalId === null) {
-      // 날짜 받아서 기록이 있는지 확인하고
-      // 있다면 기록된 시간 가져와서 거기에 더해주고
-      // 없다면 지금 방식대로 기록
-
-
-      // 공부 시작 시간 새로 받기
-      setStartTime(new Date()); // 얘가 문제였음!
-      // setInterval 시작하고 인터벌 ID를 저장
-      const id = window.setInterval(() => {
+      // 해당 날짜의 데이터가 있다면 기록된 시간 가져와서 거기에 더해주고
+      if (storedData) {
+        // 저장되어있던 시간을 불러와서
+        const parsedData = JSON.parse(storedData);
+        const storedStudyTimeInt = parsedData.studyTime ? parseInt(parsedData.studyTime, 10) : 0;
+        // 공부 시작 시간 새로 받기
+        setStartTime(new Date());
+        // setInterval 시작하고 인터벌 ID를 저장
+        const id = window.setInterval(() => {
         // 현재 시간 1초 당 계속 받아오기
         const currentTime = new Date();
-        // null 체크 빼먹지 말자,,
-        const elapsedMilliseconds = startTime ? currentTime.getTime() - startTime.getTime() + studyTime : 0;
-        // 무조건 값에 studyTime을 더해서 보여주면 되잖아...
+        // 저장되어있던 시간을 차에 더해준다.
+        const elapsedMilliseconds = startTime ? currentTime.getTime() - startTime.getTime() + storedStudyTimeInt : 0;
+        // 그리고 그 값을 elapsedTime에 넣어줌
         setElapsedTime(elapsedMilliseconds);
-      }, 1000);
-      setIntervalId(id); // 인터벌 ID 저장
+        console.log('해당 날짜의 데이터가 있습니다.')
+        console.log(elapsedMilliseconds) // 0
+        }, 1000);
+        setIntervalId(id); // 인터벌 ID 저장
+      } 
+      else {
+        setStartTime(new Date());
+        const id = window.setInterval(() => {
+        const currentTime = new Date();
+        const elapsedMilliseconds = startTime ? currentTime.getTime() - startTime.getTime() + studyTime : 0;
+        setElapsedTime(elapsedMilliseconds);
+        }, 1000);
+        setIntervalId(id); // 인터벌 ID 저장
+        console.log('해당 날짜의 데이터가 없습니다.')
+      };
     }
   };
   
@@ -49,11 +64,24 @@ const Tictoc = () => {
     // 시작 시간이 변경될 때마다 경과 시간 계산
     if (startTime !== null) {
       const interval = setInterval(() => {
-        // 때마다 시간 가지고 와서 시간 차 계산하고 elapsedTime에 넘겨주기
-        const currentTime = new Date();
-        // 계산 똑같이
-        const elapsedMilliseconds = startTime ? currentTime.getTime() - startTime.getTime() + studyTime : 0;
-        setElapsedTime(elapsedMilliseconds);
+        if (storedData) {
+          // 저장되어있던 시간을 불러와서
+          const parsedData = JSON.parse(storedData);
+          const storedStudyTimeInt = parsedData.studyTime ? parseInt(parsedData.studyTime, 10) : 0;
+          // 현재 시간 1초 당 계속 받아오기
+          const currentTime = new Date();
+          // 저장되어있던 시간을 차에 더해준다.
+          const elapsedMilliseconds = startTime ? currentTime.getTime() - startTime.getTime() + storedStudyTimeInt : 0;
+          // 그리고 그 값을 elapsedTime에 넣어줌
+          setElapsedTime(elapsedMilliseconds);
+        } else {
+          // 때마다 시간 가지고 와서 시간 차 계산하고 elapsedTime에 넘겨주기
+          const currentTime = new Date();
+          // 계산 똑같이
+          const elapsedMilliseconds = startTime ? currentTime.getTime() - startTime.getTime() + studyTime : 0;
+          setElapsedTime(elapsedMilliseconds);
+        };
+        
       }, 1000);
       return () => clearInterval(interval);
     }
@@ -66,27 +94,16 @@ const Tictoc = () => {
       clearInterval(intervalId);
       setIntervalId(null);
       
-      // 이제 해야할 것
-      // 기존에 사용하고 있던 StudyTime 대신 이 친구들을...
-      // 날짜를 받아서 어제와 같으면 같은 날짜의 데이터에 값을 저장해주고
-      // 저장한 날짜값 불러오기
-      // 현재 날짜를 문자열로 가져옵니다.
-      const currentDate = new Date().toISOString().slice(0, 10);
-
-      // localStorage에서 해당 날짜의 데이터를 가져옵니다.
-      const storedData = localStorage.getItem(currentDate);
-
+      // 해당 날짜의 데이터가 있다면
       if (storedData) {
-        // 이미 해당 날짜의 데이터가 저장되어 있는 경우
-        const parsedData = JSON.parse(storedData);
-        const storedStudyTimeInt = parsedData.studyTime ? parseInt(parsedData.studyTime, 10) : 0;
-        const updateStudyTime = storedStudyTimeInt + elapsedTime
-        // 기존 데이터를 업데이트하거나 필요에 따라 처리합니다.
-        const updateData = { "username": 'coenffl', "date": parsedData.date, "studyTime": updateStudyTime.toString() };
+        const parsedData = JSON.parse(storedData);    
+        // 저장되어있던 날짜에 새로 측정한 시간을 넣어주기만 하면 됨
+        const updateData = { ...parsedData, "studyTime": elapsedTime.toString() };
         const studyJsonString = JSON.stringify(updateData);
         localStorage.setItem(parsedData.date, studyJsonString);
       } else {
         // 해당 날짜의 데이터가 아직 없는 경우
+        // 오늘 날짜에 저장된 시간 넣어주면 됨
         const studyData = { "username": 'coenffl', "date": currentDate, "studyTime": elapsedTime.toString() };
         const studyJsonString = JSON.stringify(studyData);
         localStorage.setItem(currentDate, studyJsonString);
